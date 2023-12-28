7572f3e3ee9c76805cfa804f7d1b0660f9ba6168
zerocool <zerocool@gmail.com>
2023-12-28 13:08:48 +0600

    taking user inputs and handling errors

--- calculator.py
+++ calculator.py
@@ -21,3 +21,22 @@ def multiply(num1, num2):
 
 def divide(num1, num2):
     return num1 / num2
+
+
+print("Please select operation.")
+print("1.Add")
+print("2.Substract")
+print("3.Multiply")
+print("4.Divide")
+
+# Taking user inputs
+while True:
+    choice = (input("Enter choice -->> "))
+# check if choice is one of the four options
+    if choice in ("1", "2", "3", "4"):
+        try:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+        except ValueError:
+            print("Invalid input.Please enter a number.")
+            continue
