18bfc26a3df84f0ea6535a2a413f613ae0434045
zerocool <zerocool@gmail.com>
2023-12-28 12:54:50 +0600

    defining functions

---
+++ calculator.py
@@ -0,0 +1,23 @@
+# Python program for simple calculator
+
+# Function to add two numbers
+def add(num1, num2):
+    return num1 + num2
+
+# Function to substract two numbers
+
+
+def substract(num1, num2):
+    return num1 - num2
+
+# Function to multiply two numbers
+
+
+def multiply(num1, num2):
+    return num1 * num2
+
+# Function to divide two numbers
+
+
+def divide(num1, num2):
+    return num1 / num2
